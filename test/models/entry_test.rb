c9d4a8ef7b3ec22f25e19b39bfbd8d2feca3726f
Jason Crockett <JesusisLord33333@outlook.com>
2022-02-21 22:51:16 +0000

    added model tests

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -4,4 +4,26 @@ class EntryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  #
+
+  def test_that_blank_entries_fail
+    entry = Entry.new
+    assert_not entry.save, "entry was saved!"
+  end
+  def test_assert_just_phone_number_fails
+    entry = Entry.new({mobile_phone:"07432123456"})
+    assert_not entry.valid?
+    assert_not entry.save, "entry was saved!"
+  end
+  def test_assert_just_phone_number_and_sign_out_fails
+    entry = Entry.new({mobile_phone:"07432123456"})
+    assert_not entry.valid?
+    assert_not entry.save, "entry was saved!"
+  end
+  def test_that_filled_name_and_time_in_passes
+    # warning! this test is not tdd compliant because when no model validation is specified, the test passes.
+    entry = Entry.new({name:'jason',time_in:Time.now})
+    assert entry.valid?
+    assert entry.save
+  end
 end
