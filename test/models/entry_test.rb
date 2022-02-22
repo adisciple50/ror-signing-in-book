5d7ed61f31ded2aeb5997d09313cd9c9e1cbcfaf
Jason Crockett <JesusisLord33333@outlook.com>
2022-02-22 01:38:57 +0000

    corrected an ommision caused by exhaustion and added a new test. food helped. :D

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -12,18 +12,19 @@ class EntryTest < ActiveSupport::TestCase
   end
   def test_assert_just_phone_number_fails
     entry = Entry.new({mobile_phone:"07432123456"})
-    assert_not entry.valid?
     assert_not entry.save, "entry was saved!"
   end
   def test_assert_just_phone_number_and_sign_out_fails
-    entry = Entry.new({mobile_phone:"07432123456"})
-    assert_not entry.valid?
+    entry = Entry.new({mobile_phone:"07432123456",time_out:Time.now})
+    assert_not entry.save, "entry was saved!"
+    end
+  def test_assert_just_sign_in_and_sign_out_fails
+    entry = Entry.new({time_in:Time.now,time_out:Time.now})
     assert_not entry.save, "entry was saved!"
   end
   def test_that_filled_name_and_time_in_passes
     # warning! this test is not tdd compliant because when no model validation is specified, the test passes.
     entry = Entry.new({name:'jason',time_in:Time.now})
-    assert entry.valid?
-    assert entry.save
+    assert entry.save,"entry was not saved!"
   end
 end
