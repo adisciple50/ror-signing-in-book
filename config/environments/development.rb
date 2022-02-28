2449c1c18319bde01964459841186a7723e632a4
Jason Crockett <JesusisLord33333@outlook.com>
2022-02-28 17:19:44 +0000

    installed node js for a successful bootstrap install. all tests pass.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # enable sassc inline source maps
+  config.sass.inline_source_maps = true
+
 end
