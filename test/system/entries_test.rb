13d00ef24480b32eebbf24ace475b1350ad867e4
Jason Crockett <JesusisLord33333@outlook.com>
2022-04-16 13:20:37 +0100

    sorted login.

--- test/system/entries_test.rb
+++ test/system/entries_test.rb
@@ -3,17 +3,31 @@ require "application_system_test_case"
 class EntriesTest < ApplicationSystemTestCase
   setup do
     @entry = entries(:one)
+    @admin = admins(:one)
+  end
+
+  def login!
+    visit root_url
+    click_on "Admins"
+    fill_in "Username",with:@admin.username
+    fill_in "Password",with:"test1"
+    click_on "login"
   end
 
   test "visiting the index" do
-    visit entries_url
+    login!
+
+    click_on "View All Entries"
     assert_selector "h1", text: "Entries"
   end
 
   test "should create entry" do
+    login!
+
     visit entries_url
     click_on "New entry"
 
+    fill_in "Username", with: @entry.username
     fill_in "Mobile phone", with: @entry.mobile_phone
     fill_in "Name", with: @entry.name
     fill_in "Time in", with: @entry.time_in
@@ -25,6 +39,8 @@ class EntriesTest < ApplicationSystemTestCase
   end
 
   test "should update Entry" do
+    login!
+
     visit entry_url(@entry)
     click_on "Edit this entry", match: :first
 
@@ -39,6 +55,7 @@ class EntriesTest < ApplicationSystemTestCase
   end
 
   test "should destroy Entry" do
+    login!
     visit entry_url(@entry)
     click_on "Destroy this entry", match: :first
 
