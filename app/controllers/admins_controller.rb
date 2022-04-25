64a5fe2d2dd85e50a6890c2c5eb64540b8fb2da9
Jason Crockett <JesusisLord33333@outlook.com>
2022-04-25 12:56:05 +0100

    moved authentication to its own controller

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -59,18 +59,6 @@ class AdminsController < ApplicationController
     end
   end
 
-  #TODO - redirects to admin page regardless of successful login
-  def login
-    if login!(params["username"],params["password"])
-      redirect_to :controller => :entries,action: :index
-    else
-      redirect_to admins_url # calls this when true? whats going on!
-    end
-  end
-  def logout
-    logout!(session["username"])
-    redirect_to root_url
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -79,45 +67,5 @@ class AdminsController < ApplicationController
   end
 
     # Only allow a list of trusted parameters through.
-  def admin_params
-    params.require(:admin).permit(:username, :password,:token)
-  end
 
-  def generate_unique_token
-    return SecureRandom::uuid
-  end
-
-  # quick and dirty workaround to save and update not working
-  # only to be used for authenticated users!
-  def update_token(username,new_token)
-    ActiveRecord::Base.connection.execute("UPDATE admins SET token = '#{new_token}' WHERE username = '#{username}'")
-  end
-
-  def logout!(username)
-    ActiveRecord::Base.connection.execute("UPDATE admins SET token = NULL WHERE username = '#{username}'")
-  end
-
-  def login!(username,password)
-    user = Admin.find_by(username:username)
-    if user&.authenticate(password)
-      # eliminate dup tokens
-      unique_token = generate_unique_token
-      update_token username,unique_token # i could use user.username but i wont because i think its quicker
-      session["username"] = user.username
-      session["token"] =  unique_token
-      return true
-    else
-      return false
-    end
-
-  end
-  def log_out!(username)
-    user = Admin.find_by(username:username)
-    logged_out_token = 'logged_out'
-    if user
-      update_token user.username,logged_out_token
-      session["username"] = logged_out_token
-      session["token"] = logged_out_token
-    end
-  end
 end
